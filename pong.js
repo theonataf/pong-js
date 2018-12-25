880cbcd98680c10aa53c507e2a27bce51c9ef58e
Theo Nataf <tdh.nataf@gmail.com>
2018-12-25 15:37:23 +0200

    bouncing effect

---
+++ pong.js
@@ -0,0 +1,151 @@
+var canvas = document.getElementById("pong");
+var ctx = canvas.getContext("2d");
+var raf;
+
+var scale = 2;
+canvas.width = 1280 * scale;
+canvas.height = 739 * scale;
+
+const positions = {
+	width: canvas.width,
+	height: canvas.height,
+	width_center: canvas.width /2,
+	height_center: canvas.height /2,
+	find_center(){
+		ctx.translate(this.width_center, this.height_center);
+	},
+	replace_center() {
+		ctx.translate(-this.width_center, -this.height_center);
+	}
+}
+
+
+function testDraw() {
+    ctx.beginPath();
+    positions.find_center();
+    ctx.arc(0, 0, positions.width_center/2, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(0, 300);
+    ctx.arc(0, 50, 500, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.stroke();
+    positions.replace_center();
+  }
+
+function grid() {
+	
+	for(var i=0; i<canvas.width; i+=35){
+		ctx.beginPath();
+		ctx.moveTo(i,0);
+		ctx.lineTo(i,canvas.height);
+		ctx.stroke();
+		ctx.font = '20px serif';
+  		ctx.fillText(i/35, i, 30);
+	}
+
+	for (var i = 0; i<canvas.height; i+=35) {
+		ctx.beginPath();
+		ctx.moveTo(0,i);
+		ctx.lineTo(canvas.width,i);
+		ctx.stroke();
+		ctx.font = '20px serif';
+  		ctx.fillText(i/35, 5, i+32);
+	}
+
+}
+// arc(x,y,radius, start angle, end angle, anticlockwise)
+
+
+var Ball = {
+	x: positions.width_center,
+	y: positions.height - 75,
+	vx: -6,
+	vy: -6,
+	radius: 25,
+	color:'blue',
+	drawBall: function() {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+		ctx.closePath();
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	},
+	isTouchingBar: function() {
+		if (((this.y + this.vy) >= (Bar.y - this.radius))
+				&&(((this.x + this.vx - this.radius) > Bar.x)
+				&&((this.x + this.vx + this.radius)< (Bar.x + Bar.width)))) {
+			this.vy = -this.vy;
+			console.log('touched');
+		}
+	},
+};
+
+var Bar = {
+	x: positions.width_center - 75,
+	y: positions.height - 50,
+	vx: 10,
+	thickness: 25,
+	width: 1200,
+	color: 'red',
+	drawBar: function(){
+		ctx.beginPath();
+		ctx.rect(this.x, this.y, this.width, this.thickness);
+		ctx.closePath();
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
+};
+
+function play() {
+	ctx.clearRect(0,0, positions.width, positions.height);
+	grid();
+	Ball.drawBall();
+	Bar.drawBar();
+	if ((Ball.y + Ball.vy > positions.height - Ball.radius) || (Ball.y + Ball.vy < Ball.radius)) {
+		Ball.vy = -Ball.vy;
+	}
+	if ((Ball.x + Ball.vx > positions.width - Ball.radius) || (Ball.x + Ball.vx < Ball.radius)) {
+		Ball.vx = -Ball.vx;
+	}
+	if ((Bar.x + Bar.vx > positions.width - 150) || (Bar.x + Bar.vx < 0) ) {
+		Bar.vx = -Bar.vx;
+	}
+	Ball.isTouchingBar();
+	Ball.x += Ball.vx;
+	Ball.y += Ball.vy;
+	Bar.x += Bar.vx;
+	raf = window.requestAnimationFrame(play);
+}
+
+
+canvas.addEventListener('mouseover', function(event) {
+	raf = window.requestAnimationFrame(play);
+})
+
+canvas.addEventListener('mouseout', function(event) {
+  window.cancelAnimationFrame(raf);
+});
+
+Ball.drawBall();
+Bar.drawBar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
