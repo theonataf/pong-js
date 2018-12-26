65074d0d536005a3aa6597136b3d950b182928f4
Theo Nataf <tdh.nataf@gmail.com>
2018-12-26 14:41:54 +0200

    smooth motions

--- pong.js
+++ pong.js
@@ -19,19 +19,39 @@ const positions = {
 	}
 }
 var code;
+var release;
 window.addEventListener('keydown', function(event) {
 		event.preventDefault();
 		code = event.keyCode;
-		// if (code == 37) { // left
-		// 	console.log('left');
-		// } else if (code == 39) { // right
-		// 	console.log('right');
-		// } else if (code == 38) { // up
-		// 	console.log('up');
-		// } else if (code == 40) { // down
-		// 	console.log('down');
-		// }
+		if (code == 37) { // left
+			console.log('left');
+			Bar.direction = 'left';
+			Bar.leftKeyDown = true;
+		} else if (code == 39) { // right
+			console.log('right');
+			Bar.direction = 'right';
+			Bar.rightKeyDown = true;
+		} else if (code == 38) { // up
+			console.log('up');
+		} else if (code == 40) { // down
+			console.log('down');
+		}
 });
+window.addEventListener('keyup', function(event) {
+	event.preventDefault();
+	release = event.keyCode;
+	if (release == 37) { // left
+			console.log('leftup');
+			Bar.leftKeyDown = false;
+		} else if (release == 39) { // right
+			console.log('rightup');
+			Bar.rightKeyDown = false;
+		} else if (release == 38) { // up
+			console.log('upup');
+		} else if (release == 40) { // down
+			console.log('downup');
+		}
+})
 
 function grid() {
 	
@@ -56,12 +76,15 @@ function grid() {
 }
 // arc(x,y,radius, start angle, end angle, anticlockwise)
 
+var Game = {
+	status: 'on',
+};
 
 var Ball = {
 	x: positions.width_center,
 	y: positions.height - 80,
-	vx: -6,
-	vy: -6,
+	vx: -10,
+	vy: -10,
 	radius: 25,
 	color:'blue',
 	drawBall: function() {
@@ -82,11 +105,19 @@ var Ball = {
 	isTouchingBorder: function() {
 		if ((Ball.y + Ball.vy < Ball.radius)) {
 		Ball.vy = -Ball.vy;
-		}
+		};
 		if ((Ball.x + Ball.vx > positions.width - Ball.radius) || (Ball.x + Ball.vx < Ball.radius)) {
 		Ball.vx = -Ball.vx;
 		}
-		// if (Ball.y + Ball.vy > positions.height - Ball.radius) then game over
+		if (Ball.y + Ball.vy > positions.height - Ball.radius) {
+			Game.status = 'gameover';
+			Ball.vx = 0;
+			Ball.vy = 0;
+			Bar.vx = 0;
+			Bar.speed = 0;
+			console.log('gameover');
+			alert('gameover');
+		}
 	},
 };
 
@@ -94,9 +125,13 @@ var Bar = {
 	x: positions.width_center - 150,
 	y: positions.height - 55,
 	vx: 10,
+	speed: 17,
 	thickness: 25,
 	width: 300,
 	color: 'red',
+	direction: null,
+	leftKeyDown: false,
+	rightKeyDown: false,
 	drawBar: function(){
 		ctx.beginPath();
 		ctx.rect(this.x, this.y, this.width, this.thickness);
@@ -105,30 +140,24 @@ var Bar = {
 		ctx.fill();
 	},
 	isTouchingBorder: function() {
-		if ((Bar.x + Bar.vx > positions.width - Bar.width) || (Bar.x + Bar.vx < 0) ) {
-			Bar.vx = 0;
+		if ((this.x + this.vx > positions.width - this.width) || (this.x + this.vx < 0) ) {
+			this.vx = 0;
 		};
 	},
 	motion: function() {
 		// must use 'keydown' for arrow keys
 		// tomorrow !!!! on 'keyup' stop the bar 
-		var speed = 17;
-		if (code == 37) {
-			if(speed > 0){
-				this.vx = -speed;
-			}
-			if(speed < 0){
-				this.vx = speed;
-			}
+		this.isTouchingBorder();
+		if (this.direction == 'left' && this.leftKeyDown == true) {
+			this.vx = -this.speed;
+			Bar.x += Bar.vx;
 		}
-		if (code == 39) {
-			if(speed < 0){
-				this.vx = -speed;
-			}
-			if(speed > 0){
-				this.vx = speed;
-			}
+		this.isTouchingBorder();
+		if (this.direction == 'right' && this.rightKeyDown == true) {
+			this.vx = this.speed;
+			Bar.x += Bar.vx;
 		}
+
 	},
 };
 
@@ -137,27 +166,29 @@ function play() {
 	grid();
 	Ball.drawBall();
 	Bar.drawBar();
-	Bar.motion();
 	Bar.isTouchingBorder();
+	Bar.motion();
 	Ball.isTouchingBorder();
 	Ball.isTouchingBar();
 	Ball.x += Ball.vx;
 	Ball.y += Ball.vy;
-	Bar.x += Bar.vx;
 	raf = window.requestAnimationFrame(play);
 }
 
 
-canvas.addEventListener('mouseover', function(event) {
-	raf = window.requestAnimationFrame(play);
-})
 
-canvas.addEventListener('mouseout', function(event) {
-  window.cancelAnimationFrame(raf);
-});
+
 
 Ball.drawBall();
 Bar.drawBar();
+
+if (Game.status == 'gameover') {
+	console.log('score');
+}
+play();
+
+
+
 
 
 
